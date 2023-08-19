dd59e57df76962df85254f63479eed3887f84114
Dmitry Kralichkin <dm_krl@mail.ru>
2023-08-19 14:31:43 +0400

    Done hw-2

--- components/renderMainPage.js
+++ components/renderMainPage.js
@@ -1,11 +1,12 @@
+import renderGamePage from './renderGamePage.js';
 const app = document.querySelector('.app');
 
 function renderMainPage() {
   const mainPageHtml = `
-  <form class="main">
+<div class="main">
   <p class="main-text">Выберите сложность</p>
   <div class="main-checbox">
-      <input type="radio" id="es" name="difficulty" /><label
+      <input type="radio" id="es" name="difficulty"/><label
         for="es"
         class="main-checbox-text"
         >1</label
@@ -22,10 +23,21 @@ function renderMainPage() {
       >
   </div>
   <button class="main-start">Старт</button>
-</form>
+</div>
 `;
-
   app.innerHTML = mainPageHtml;
+  const boxSelectedDifficulty = document.querySelector('.main');
+
+  boxSelectedDifficulty.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.closest('.main-checbox-text')) {
+      localStorage.setItem('checked', target.getAttribute('for'));
+    }
+    if (target.closest('.main-start')) {
+      let levelDifficulty = localStorage.getItem('checked');
+      renderGamePage(levelDifficulty);
+    }
+  });
 }
 
 export { renderMainPage };
